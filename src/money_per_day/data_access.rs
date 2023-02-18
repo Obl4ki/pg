1403ef6903aa5d9256d981e75f09f16d2bdb2919
Obl4ki <lukaszoblak33@gmail.com>
2023-02-18 01:18:01 +0100

    Add hard error handling

--- src/money_per_day/data_access.rs
+++ src/money_per_day/data_access.rs
@@ -1,33 +1,52 @@
 use crate::money_per_day::locale::Lang;
 use std::io::stdin;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum InputError {
+    #[error("Failed reading from stdin")]
+    StdinError,
+    #[error("Failed parsing the day number {line} - not a number")]
+    DayInputNotANumber { line: String },
+    #[error("Money amount should be a non-negative integer, got {amount} instead")]
+    MoneyReadLineError { amount: String },
+}
+
 #[derive(Clone, Copy, Debug)]
 pub struct UserInputData {
     pub payout_day_of_month: u32,
     pub money_amount: u32,
 }
 
-pub fn from_stdin<L: Lang>() -> Result<UserInputData, String> {
+pub fn from_stdin<L: Lang>() -> Result<UserInputData, InputError> {
     println!("{}", L::input_payday_number());
-    let mut payout_day = String::new();
-    stdin().read_line(&mut payout_day).map_err(|_| {
-        format!("Failed parsing the line {payout_day}, expected non-negative number.")
-    })?;
+    let mut payout_day_line = String::new();
+    stdin()
+        .read_line(&mut payout_day_line)
+        .map_err(|_| InputError::StdinError)?;
 
-    let payout_day_of_month = payout_day.trim().parse().map_err(|_| {
-        format!("{payout_day} should be a non-negative integer representing day of month.")
-    })?;
+    let payout_day_of_month =
+        payout_day_line
+            .trim()
+            .parse()
+            .map_err(|_| InputError::DayInputNotANumber {
+                line: payout_day_line,
+            })?;
 
     println!("{}", L::input_account_balance());
-    let mut money_amount = String::new();
-    stdin().read_line(&mut money_amount).map_err(|_| {
-        format!("Failed parsing the line {money_amount}, expected non-negative number.")
-    })?;
 
-    let money_amount = money_amount.trim().parse().map_err(|_| {
-        format!(
-        "{payout_day} should be a non-negative integer representing the amount of money you have."
-    )
-    })?;
+    let mut money_amount_line = String::new();
+    stdin()
+        .read_line(&mut money_amount_line)
+        .map_err(|_| InputError::StdinError)?;
+
+    let money_amount =
+        money_amount_line
+            .trim()
+            .parse()
+            .map_err(|_| InputError::MoneyReadLineError {
+                amount: money_amount_line,
+            })?;
 
     Ok(UserInputData {
         payout_day_of_month,
